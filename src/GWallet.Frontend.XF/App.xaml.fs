6e8ff47e20871d722559f34a30ecf1241a00e0ef
Andres G. Aragoneses <knocte@gmail.com>
2018-08-21 16:41:59 +0800

    Frontend.XF: cosmetic, move types&modules to their own fille

--- src/GWallet.Frontend.XF/App.xaml.fs
+++ src/GWallet.Frontend.XF/App.xaml.fs
@@ -1,42 +1,6 @@
 ﻿namespace GWallet.Frontend.XF
 
-open System
-open System.Linq
-
 open Xamarin.Forms
-
-open GWallet.Backend
-
-type GlobalState() =
-    let lockObject = Object()
-    let mutable awake = true
-    member internal this.Awake
-        with set value = lock lockObject (fun _ -> awake <- value)
-
-    interface FrontendHelpers.IGlobalAppState with
-        member this.Awake
-            with get() = lock lockObject (fun _ -> awake)
-
-module Initialization =
-
-    let internal GlobalState = GlobalState()
-
-    let private GlobalInit () =
-        Infrastructure.SetupSentryHook ()
-
-    let internal LandingPage(): NavigationPage =
-        let state = GlobalInit ()
-
-        let accounts = Account.GetAllActiveAccounts()
-        let landingPage:Page =
-            if not (accounts.Any()) then
-                (WelcomePage GlobalState) :> Page
-            else
-                (LoadingPage GlobalState) :> Page
-
-        let navPage = NavigationPage landingPage
-        NavigationPage.SetHasNavigationBar(landingPage, false)
-        navPage
 
 type App() =
     inherit Application(MainPage = Initialization.LandingPage())
---
+++ src/GWallet.Frontend.XF/GlobalState.fs
@@ -0,0 +1,13 @@
+﻿namespace GWallet.Frontend.XF
+
+open System
+
+type GlobalState() =
+    let lockObject = Object()
+    let mutable awake = true
+    member internal this.Awake
+        with set value = lock lockObject (fun _ -> awake <- value)
+
+    interface FrontendHelpers.IGlobalAppState with
+        member this.Awake
+            with get() = lock lockObject (fun _ -> awake)
